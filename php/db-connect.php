f0463b39254a9991b71d67cd497c74bcabf1d6a7
Alem <106407795+Alem569@users.noreply.github.com>
2025-01-17 11:01:33 +0100

    chore: add database structure

--- php/db-connect.php
+++ php/db-connect.php
@@ -4,7 +4,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "quiz";
-$port = 3307;
+$port = 3306;
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$database;port=$port", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
