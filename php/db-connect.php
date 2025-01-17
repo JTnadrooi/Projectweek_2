ce655fb280c7d59dd84f2ac5100b787c5b73ee82
Alem <106407795+Alem569@users.noreply.github.com>
2025-01-17 10:49:29 +0100

    chore: fix security

--- php/db-connect.php
+++ php/db-connect.php
@@ -9,6 +9,6 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$database;port=$port", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    header('Location: index.html?error=db');
+    header('Location: error.php?error=db');
     die();
 }
